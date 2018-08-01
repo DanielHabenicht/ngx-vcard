8ff3e9848d1e7711e87dd2fa59e3551990b745db
Daniel Habenicht <daniel-habenicht@outlook.de>
2018-08-01 11:19:04 +0200

    test(App Test): Fix not imported Module to test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NgxVcardModule } from 'ngx-vcard';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [NgxVcardModule]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
