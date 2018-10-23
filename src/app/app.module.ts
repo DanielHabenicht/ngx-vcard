468613de47eeffeb8736183cd434e6e14af72979
Daniel Habenicht <daniel-habenicht@outlook.de>
2018-10-23 11:44:31 +0200

    chore(Stackblitz): Fix package not found error on stackblitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgxVcardModule } from 'ngx-vcard';
+// ATTENTION: This is for development only!
+// For production / use as package use this line:
+// import { NgxVcardModule } from 'ngx-vcard';
+import { NgxVcardModule } from 'projects/ngx-vcard/src/lib/ngx-vcard.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgxVcardModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxVcardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
