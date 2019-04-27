ef14c07f1f7451ab43e61e25ae1d39b3e4426a70
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-04-27 13:07:45 +0200

    Update tests

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,40 +1,36 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+const { SpecReporter } = require('jasmine-spec-reporter');
+const { JUnitXmlReporter } = require('jasmine-reporters');
 
-const { SpecReporter } = require("jasmine-spec-reporter");
-
-var path = require("path");
-var downloadsPath = path.resolve(__dirname, "./downloads");
+process.env.CHROME_BIN = process.env.CHROME_BIN || require('puppeteer').executablePath();
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: ["./src/**/*.e2e-spec.ts"],
+  specs: ['./src/**/*.e2e-spec.ts'],
   capabilities: {
-    browserName: "chrome",
+    browserName: 'chrome',
+
     chromeOptions: {
-      args: ["--no-sandbox", "--test-type=browser", "--disable-gpu"],
-      prefs: {
-        "plugins.always_open_pdf_externally": true,
-        download: {
-          prompt_for_download: false,
-          default_directory: downloadsPath,
-          directory_upgrade: true
-        }
-      }
+      args: ['--headless', '--disable-gpu', '--window-size=1200,900'],
+      binary: process.env.CHROME_BIN
     }
   },
   directConnect: true,
-  baseUrl: "http://localhost:4200/",
-  framework: "jasmine",
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
   onPrepare() {
-    require("ts-node").register({
-      project: require("path").join(__dirname, "./tsconfig.e2e.json")
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    var junitReporter = new JUnitXmlReporter({
+      savePath: require('path').join(__dirname, './junit'),
+      consolidateAll: true
+    });
+    jasmine.getEnv().addReporter(junitReporter);
   }
 };
